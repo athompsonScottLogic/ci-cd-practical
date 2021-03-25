59bad2e5e25261c4d7dc45b76b2a4c08fb708783
Alexander Thompson <you@example.com>
2021-03-25 11:43:45 +0000

    counterdown added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Counter from './Counter';
+import CounterDown from './CounterDown';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CounterDown />
   </React.StrictMode>,
   document.getElementById('root')
 );
